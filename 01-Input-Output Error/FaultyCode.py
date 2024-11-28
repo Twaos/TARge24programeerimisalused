21aff13c41d7db58bc9fa701591cb57621024d50
Freddie <freddie-anton@hotmail.com>
2024-11-28 17:25:43 +0200

    Demonstrate common errors in python code

---
+++ 01-Input-Output Error/FaultyCode.py
@@ -0,0 +1,13 @@
+# print("Hello World) # Jutumärgid lõpetamata
+
+
+# print("Hello World") #Taande viga
+
+# if __name__ == '__main__':
+#    print("Hello World")
+
+# print(undclared_variable + 10)  # --> Nameerror
+
+
+variable = "viis"
+print(variable + 10)  # typeerror :
